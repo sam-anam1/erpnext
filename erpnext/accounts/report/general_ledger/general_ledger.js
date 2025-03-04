2c7395a7dcc5c508d8e899fcb1e96710de395269
Sam <6969182+sam-anam1@users.noreply.github.com>
2025-03-04 11:46:08 -0800

    Update general_ledger.js

--- erpnext/accounts/report/general_ledger/general_ledger.js
+++ erpnext/accounts/report/general_ledger/general_ledger.js
@@ -4,60 +4,6 @@
 frappe.query_reports["General Ledger"] = {
 	filters: [
 		{
-			fieldname: "company",
-			label: __("Company"),
-			fieldtype: "Link",
-			options: "Company",
-			default: frappe.defaults.get_user_default("Company"),
-			reqd: 1,
-		},
-		{
-			fieldname: "finance_book",
-			label: __("Finance Book"),
-			fieldtype: "Link",
-			options: "Finance Book",
-		},
-		{
-			fieldname: "from_date",
-			label: __("From Date"),
-			fieldtype: "Date",
-			default: frappe.datetime.add_months(frappe.datetime.get_today(), -1),
-			reqd: 1,
-			width: "60px",
-		},
-		{
-			fieldname: "to_date",
-			label: __("To Date"),
-			fieldtype: "Date",
-			default: frappe.datetime.get_today(),
-			reqd: 1,
-			width: "60px",
-		},
-		{
-			fieldname: "account",
-			label: __("Account"),
-			fieldtype: "MultiSelectList",
-			options: "Account",
-			get_data: function (txt) {
-				return frappe.db.get_link_options("Account", txt, {
-					company: frappe.query_report.get_filter_value("company"),
-				});
-			},
-		},
-		{
-			fieldname: "voucher_no",
-			label: __("Voucher No"),
-			fieldtype: "Data",
-			on_change: function () {
-				frappe.query_report.set_filter_value("group_by", "Group by Voucher (Consolidated)");
-			},
-		},
-		{
-			fieldname: "against_voucher_no",
-			label: __("Against Voucher No"),
-			fieldtype: "Data",
-		},
-		{
 			fieldtype: "Break",
 		},
 		{
@@ -111,6 +57,62 @@ frappe.query_reports["General Ledger"] = {
 			fieldtype: "Data",
 			hidden: 1,
 		},
+		
+		{
+			fieldname: "company",
+			label: __("Company"),
+			fieldtype: "Link",
+			options: "Company",
+			default: frappe.defaults.get_user_default("Company"),
+			reqd: 1,
+		},
+		{
+			fieldname: "finance_book",
+			label: __("Finance Book"),
+			fieldtype: "Link",
+			options: "Finance Book",
+		},
+		{
+			fieldname: "from_date",
+			label: __("From Date"),
+			fieldtype: "Date",
+			default: frappe.datetime.add_months(frappe.datetime.get_today(), -1),
+			reqd: 1,
+			width: "60px",
+		},
+		{
+			fieldname: "to_date",
+			label: __("To Date"),
+			fieldtype: "Date",
+			default: frappe.datetime.get_today(),
+			reqd: 1,
+			width: "60px",
+		},
+		{
+			fieldname: "account",
+			label: __("Account"),
+			fieldtype: "MultiSelectList",
+			options: "Account",
+			get_data: function (txt) {
+				return frappe.db.get_link_options("Account", txt, {
+					company: frappe.query_report.get_filter_value("company"),
+				});
+			},
+		},
+		{
+			fieldname: "voucher_no",
+			label: __("Voucher No"),
+			fieldtype: "Data",
+			on_change: function () {
+				frappe.query_report.set_filter_value("group_by", "Group by Voucher (Consolidated)");
+			},
+		},
+		{
+			fieldname: "against_voucher_no",
+			label: __("Against Voucher No"),
+			fieldtype: "Data",
+		},
+		
 		{
 			fieldname: "group_by",
 			label: __("Group by"),
@@ -206,6 +208,7 @@ frappe.query_reports["General Ledger"] = {
 			fieldname: "show_remarks",
 			label: __("Show Remarks"),
 			fieldtype: "Check",
+			default: 1,
 		},
 		{
 			fieldname: "ignore_err",
